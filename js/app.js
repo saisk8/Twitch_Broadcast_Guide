430db2b5cd4d1defc34a92cfc2b8f1fd11882861
Sai Somanath.K <k.saisomanath@gmail.com>
2017-06-16 19:04:32 +0530

    Add the API call

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 var channel_list = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 function getinfo() {
-    channel_list.forEach(function() {
+    channel_list.forEach(function(c) {
+        var url = "https://wind-bow.gomix.me/twitch-api/streams/" + c + "/?callback=?";
         $.getJSON(url, function(data) {
-            console.log(dat);
+            console.log(data);
         });
     });
 }
+
+$(document)
+    .ready(getinfo);
